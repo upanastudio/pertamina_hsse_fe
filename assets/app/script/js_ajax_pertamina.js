c531c56face6d1c6c55e4ec0f2a83300ec63aa2e
Ahmad Rifaldi <aldi4rifaldi@gmail.com>
2019-06-28 10:31:29 +0800

    update admin depot

--- assets/app/script/js_ajax_pertamina.js
+++ assets/app/script/js_ajax_pertamina.js
@@ -33,8 +33,8 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 			columns: [
 			{data: 'no'},
 			{data: 'nama_pekerja'},
+			{data: 'kategori'},
 			{data: 'perusahaan'},
-			{data: 'status'},
 			{data: 'aksi'},
 			],
 
@@ -54,26 +54,14 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 				orderable: false,
 				render: function(data, type, full, meta) {
 					return `
-					<a href="rincian_data_pekerja_baru.html" class="btn btn-sm btn-primary" style="color:white;border-radius:20px">Rincian</a>`;
+					<a href="rincian_data_pekerja.html" class="btn btn-sm btn-primary" style="color:white;border-radius:20px">Rincian</a>` +
+					'<a data-toggle="modal" data-target="#terima" class="btn btn-sm btn-success" style="color:white;border-radius:20px">Terima</a>' +
+					'<a data-toggle="modal" data-target="#tolak" class="btn btn-sm btn-danger" style="color:white;border-radius:20px">Tolak</a>';
 				},
 			},
 			{
 				targets: [0, 1, 2, 3, 4],
 				className: 'text-center'
-			},
-			{
-				targets: -2,
-				width: 220,
-				render: function(data, type, full, meta) {
-					var status = {
-						Aktif: {'title': 'Aktif', 'class': ' btn-label-success'},
-						Tidak: {'title': 'Tidak Aktif', 'class': 'btn-label-danger'},
-					};
-					if (typeof status[data] === 'undefined') {
-						return data;
-					}
-					return '<span class="btn btn-bold btn-sm btn-font-sm ' + status[data].class + '">' + status[data].title + '</span>';
-				},
 			},
 			],
 		});
@@ -582,7 +570,7 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 				orderable: false,
 				render: function(data, type, full, meta) {
 					return `
-					<a href="rincian_data_pekerja_baru.html" class="btn btn-sm btn-primary" style="color:white;border-radius:20px">Rincian</a>`;
+					<a href="rincian_data_pekerja.html" class="btn btn-sm btn-primary" style="color:white;border-radius:20px">Rincian</a>`;
 				},
 			},
 			{
@@ -703,7 +691,7 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 				orderable: false,
 				render: function(data, type, full, meta) {
 					return `
-					<a href="rincian_data_pekerja_baru.html" class="btn btn-sm btn-primary" style="color:white;border-radius:20px">Rincian</a>`;
+					<a href="rincian_data_pekerja.html" class="btn btn-sm btn-primary" style="color:white;border-radius:20px">Rincian</a>`;
 				},
 			},
 			{
@@ -774,6 +762,127 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 		});
 
 	};
+	var initTable8 = function() {
+		// begin first table
+		var table = $('#tbl_input_data_pekerja').DataTable({
+			responsive: true,
+			// Pagination settings
+			dom: `<'row'<'col-sm-12'tr>>
+			<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+			// read more: https://datatables.net/examples/basic_init/dom.html
+
+			lengthMenu: [5, 10, 25, 50],
+
+			pageLength: 10,
+
+			language: {
+				'lengthMenu': 'Display _MENU_',
+			},
+
+			searchDelay: 500,
+			processing: true,
+			serverSide: true,
+			ajax: {
+				url: '../source/input_data_pekerja_baru.json',
+				type: 'POST',
+				
+			},
+			columns: [
+			{data: 'no'},
+			{data: 'nama_pekerja'},
+			{data: 'no_id'},
+			{data: 'perusahaan'},
+			{data: 'status'},
+			{data: 'aksi'},
+			],
+
+			initComplete: function() {
+				this.api().columns().every(function() {
+					var column = this;
+
+
+				});
+			},
+
+			columnDefs: [
+			{
+				targets: -1,
+				title: 'Aksi',
+				className: 'text-center',
+				orderable: false,
+				render: function(data, type, full, meta) {
+					return `
+					<a href="rincian_data_pekerja.html" class="btn btn-sm btn-primary" style="color:white;border-radius:20px">Rincian</a>`;
+				},
+			},
+			{
+				targets: [0, 1, 2, 3, 4,5],
+				className: 'text-center'
+			},
+			{
+				targets: -2,
+				width: 220,
+				render: function(data, type, full, meta) {
+					var status = {
+						Aktif: {'title': 'Aktif', 'class': ' btn-label-success'},
+						Kadaluarsa: {'title': 'Kadaluarsa', 'class': 'btn-label-warning'},
+					};
+					if (typeof status[data] === 'undefined') {
+						return data;
+					}
+					return '<span class="btn btn-bold btn-sm btn-font-sm ' + status[data].class + '">' + status[data].title + '</span>';
+				},
+			},
+			],
+		});
+
+		var filter = function() {
+			var val = $.fn.dataTable.util.escapeRegex($(this).val());
+			table.column($(this).data('col-index')).search(val ? val : '', false, false).draw();
+		};
+
+		var asdasd = function(value, index) {
+			var val = $.fn.dataTable.util.escapeRegex(value);
+			table.column(index).search(val ? val : '', false, true);
+		};
+
+		$('#kt_search').on('click', function(e) {
+			e.preventDefault();
+			var params = {};
+			$('.kt-input').each(function() {
+				var i = $(this).data('col-index');
+				if (params[i]) {
+					params[i] += '|' + $(this).val();
+				}
+				else {
+					params[i] = $(this).val();
+				}
+			});
+			$.each(params, function(i, val) {
+				// apply search params to datatable
+				table.column(i).search(val ? val : '', false, false);
+			});
+			table.table().draw();
+		});
+
+		$('#kt_reset').on('click', function(e) {
+			e.preventDefault();
+			$('.kt-input').each(function() {
+				$(this).val('');
+				table.column($(this).data('col-index')).search('', false, false);
+			});
+			table.table().draw();
+		});
+
+		$('#kt_datepicker').datepicker({
+			todayHighlight: true,
+			templates: {
+				leftArrow: '<i class="la la-angle-left"></i>',
+				rightArrow: '<i class="la la-angle-right"></i>',
+			},
+		});
+
+	};
 	return {
 
 		//main function to initiate the module
@@ -785,6 +894,7 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 			initTable5();
 			initTable6();
 			initTable7();
+			initTable8();
 		},
 
 	};
