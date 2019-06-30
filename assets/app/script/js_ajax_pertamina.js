d5e6bf1409c7b1dd91222477711ef4e76e6b7647
Ahmad Rifaldi <aldi4rifaldi@gmail.com>
2019-06-30 20:22:40 +0800

    update user admin depot dan publik

--- assets/app/script/js_ajax_pertamina.js
+++ assets/app/script/js_ajax_pertamina.js
@@ -353,8 +353,8 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 				width: 220,
 				render: function(data, type, full, meta) {
 					var status = {
-						running: {'title': 'Running', 'class': ' btn-label-danger'},
-						done: {'title': 'Done', 'class': 'btn-label-success'},
+						berjalan: {'title': 'Sedang Berjalan', 'class': ' btn-label-danger'},
+						selesai: {'title': 'Selesai', 'class': 'btn-label-success'},
 					};
 					if (typeof status[data] === 'undefined') {
 						return data;
@@ -883,6 +883,111 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 		});
 
 	};
+	var initTable9 = function() {
+		// begin first table
+		var table = $('#tbl_list_sertifikat').DataTable({
+			responsive: true,
+			// Pagination settings
+			dom: `<'row'<'col-sm-12'tr>>
+			<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+			// read more: https://datatables.net/examples/basic_init/dom.html
+
+			lengthMenu: [5, 10, 25, 50],
+
+			pageLength: 10,
+
+			language: {
+				'lengthMenu': 'Display _MENU_',
+			},
+
+			searchDelay: 500,
+			processing: true,
+			serverSide: true,
+			ajax: {
+				url: '../source/tbl_list_sertifikat.json',
+				type: 'POST',
+				
+			},
+			columns: [
+			{data: 'no'},
+			{data: 'jenis_sertifikat'},
+			{data: 'expired_date_sertifikat'},
+			{data: 'aksi'},
+			],
+
+			initComplete: function() {
+				this.api().columns().every(function() {
+					var column = this;
+
+
+				});
+			},
+
+			columnDefs: [
+			{
+				targets: -1,
+				title: 'Aksi',
+				className: 'text-center',
+				orderable: false,
+				render: function(data, type, full, meta) {
+					return `
+					<a href="#" target="_blank" class="btn btn-sm btn-warning" style="border-radius:20px">Preview</a>`;
+				},
+			},
+			{
+				targets: [0, 1, 2, 3],
+				className: 'text-center'
+			},
+			],
+		});
+
+		var filter = function() {
+			var val = $.fn.dataTable.util.escapeRegex($(this).val());
+			table.column($(this).data('col-index')).search(val ? val : '', false, false).draw();
+		};
+
+		var asdasd = function(value, index) {
+			var val = $.fn.dataTable.util.escapeRegex(value);
+			table.column(index).search(val ? val : '', false, true);
+		};
+
+		$('#kt_search').on('click', function(e) {
+			e.preventDefault();
+			var params = {};
+			$('.kt-input').each(function() {
+				var i = $(this).data('col-index');
+				if (params[i]) {
+					params[i] += '|' + $(this).val();
+				}
+				else {
+					params[i] = $(this).val();
+				}
+			});
+			$.each(params, function(i, val) {
+				// apply search params to datatable
+				table.column(i).search(val ? val : '', false, false);
+			});
+			table.table().draw();
+		});
+
+		$('#kt_reset').on('click', function(e) {
+			e.preventDefault();
+			$('.kt-input').each(function() {
+				$(this).val('');
+				table.column($(this).data('col-index')).search('', false, false);
+			});
+			table.table().draw();
+		});
+
+		$('#kt_datepicker').datepicker({
+			todayHighlight: true,
+			templates: {
+				leftArrow: '<i class="la la-angle-left"></i>',
+				rightArrow: '<i class="la la-angle-right"></i>',
+			},
+		});
+
+	};
 	return {
 
 		//main function to initiate the module
@@ -895,6 +1000,7 @@ var KTDatatablesSearchOptionsAdvancedSearch = function() {
 			initTable6();
 			initTable7();
 			initTable8();
+			initTable9();
 		},
 
 	};
